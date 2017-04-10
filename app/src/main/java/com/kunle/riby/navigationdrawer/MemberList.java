48b938ba5f61377feced2beeb5e29c98dcdd69f0
LakunleD <olakunledosunmu@gmail.com>
2017-04-10 10:37:06 +0100

    Navigation Drawer

---
+++ app/src/main/java/com/kunle/riby/navigationdrawer/MemberList.java
@@ -0,0 +1,26 @@
+package com.kunle.riby.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by olakunle on 4/9/17.
+ */
+
+public class MemberList extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.member_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Member List");
+    }
+}
